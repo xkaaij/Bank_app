e54e0ae66131057a7ab1920b751467c118e0c43d
xkaaij <xander.kaaij@memocom.nl>
2015-05-26 11:40:10 +0200

    First commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Bankapp::Application.config.secret_token = 'cb9832f5b069680dae87cada1039a4827bb319e95c06e5c97eb12bd46a11217ea7e3e9ffd11d4ef5ef9a390f37356e611be83fe5bafbdaba3b30042815394ba7'
