0edc4bc153c0bac7c42377a07c2f3419eb7b1340
xkaaij <xander.kaaij@memocom.nl>
2015-05-27 16:02:00 +0200

    Refactoring queries

--- app/controllers/transactions_controller.rb
+++ app/controllers/transactions_controller.rb
@@ -11,12 +11,12 @@ class TransactionsController < ApplicationController
         @transaction.sender_account_id = current_user.account.id
         
 
-        @account = Account.where(:id => @transaction.sender_account_id).first
+        @account = Account.find (@transaction.sender_account_id)
 
         @account.amount = @account.amount - @transaction.amount
         @account.save!
 
-        @account = Account.where(:id => @transaction.receiver_account_id).first
+        @account = Account.find (@transaction.receiver_account_id)
 
         @account.amount = @account.amount + @transaction.amount
         @account.save!
